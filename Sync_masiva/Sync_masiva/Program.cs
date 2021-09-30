ff1328cb259ad9f427c1070fcad92bcd77999e5a
Matias.Rosenberg <matias@prodytec.net>
2021-09-30 11:38:12 -0300

    Listo sync masiva

---
+++ Sync_masiva/Sync_masiva/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Sync_masiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection cnn = Base_de_datos.Conexionbd.DbConnection.getDBConnection();
+
+                DataSet ds = new DataSet();
+                string Csql = "Ecomm_Syncproductos";
+                SqlDataAdapter da = new SqlDataAdapter(Csql, cnn);
+                da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                da.Fill(ds, "Csql");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro el script a ejecutar");
+            }
+        }
+    }
+}
